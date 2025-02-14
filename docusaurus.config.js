9400370657d33d54782ee5add8400a67f603b63c
Shizanit <65107731+Kitanit@users.noreply.github.com>
2025-02-14 12:55:44 +0000

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,7 @@ const config = {
       navbar: {
         title: "Docs UA Fediland",
         items: [
+          { to: "/", label: "Home", position: "left" },
           {
             "aria-label": "GitHub Repository",
             className: "navbar--github-link",
